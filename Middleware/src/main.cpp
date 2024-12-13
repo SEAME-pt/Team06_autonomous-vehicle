a75ed19744240503eaeb406837a6764fde16be54
zeira42 <zeira42@proton.me>
2024-12-13 16:07:02 +0000

    cicd test

--- Middleware/src/main.cpp
+++ Middleware/src/main.cpp
@@ -19,7 +19,14 @@ int main() {
 
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "Battery Level: " << middleware.getBatteryLevel() << std::endl;
+        auto info = batterySensor.get_battery_info();
+        std::cout << "Valor ADC: " << info["raw_adc"] << "\n";
+        std::cout << "Tensão ADC: " << info["adc_voltage"] << "V\n";
+        std::cout << "Tensão Total: " << info["voltage"] << "V\n";
+        std::cout << "Tensão por célula: " << info["cell_voltages"] << "V\n";
+        std::cout << "Bateria: " << info["percentage"] << "%\n";
+        std::cout << "Status: " << battery.getStatus(info["voltage"]) << "\n";
+        std::cout << "--------------------\n";
     }
     middleware.stop();
 

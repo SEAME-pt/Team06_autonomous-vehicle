b5cf5940de0e8f4c80bf103e7df8bf99168da93d
zeira42 <zeira42@proton.me>
2025-01-10 14:05:51 +0000

    CMake and CICD update

--- Middleware/src/main.cpp
+++ Middleware/src/main.cpp
@@ -6,21 +6,11 @@
 int main() {
     int update_interval_ms = 100; // 0.1 second
     std::string zmq_address = "tcp://*:5555";
-
-    // Create Middleware
     Middleware middleware(update_interval_ms, zmq_address);
-
-    // Create and add sensors
-    Battery battery("Main Battery");
+    Battery battery("battery");
     middleware.addSensor("battery", &battery);
-
-    // Start Middleware
     middleware.start();
-
-    // Allow it to run for 10 seconds
     std::this_thread::sleep_for(std::chrono::seconds(10));
-
-    // Stop Middleware
     middleware.stop();
 
     return 0;

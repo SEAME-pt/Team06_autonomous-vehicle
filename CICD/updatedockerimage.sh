acad9588bbac7ab762dd5794b78c2689ea77f895
SomeMedtnerGuy <nuno.sousa@seame.pt>
2025-01-23 11:53:52 +0000

    Deleted everything not belonging to branch

--- CICD/updatedockerimage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker build --rm -t jmoreiraseame/jetson-nano-ubuntu:bionic --platform Linux/ARM64 .
-docker push jmoreiraseame/jetson-nano-ubuntu:bionic
